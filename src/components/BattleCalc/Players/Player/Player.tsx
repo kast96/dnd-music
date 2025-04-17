173e70bfbaaf3450fa6d4badc5f8ff7360252195
kast96 <kast96@yandex.ru>
2025-04-17 16:19:28 +0300

    fix name text

--- src/components/BattleCalc/Players/Player/Player.tsx
+++ src/components/BattleCalc/Players/Player/Player.tsx
@@ -14,7 +14,7 @@ type PropsType = {
 export const Player: FC<PropsType> = ({register, index, remove}) => {
 	return (
 		<div className={s.container}>
-			<input className={[s.name, 'input input-border'].join(' ')} {...register(`players.${index}.name`)} placeholder='Иня' />
+			<input className={[s.name, 'input input-border'].join(' ')} {...register(`players.${index}.name`)} placeholder='Имя' />
 			<div className={s.header}>
 				<div className={s.header_item}>
 					<FaHeart className={s.header_icon} />
